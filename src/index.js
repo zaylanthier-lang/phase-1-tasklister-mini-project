50bfa3058eb548991886de0cda7b4b18104c4d02
Zayden <zaylanthier@gmail.com>
2026-03-18 10:41:59 -0400

    Finished Code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const task = document.getElementById("new-task-description").value;
+    buildToDo(task);
+  });
 });
+
+function buildToDo(task) {
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  const ul = document.getElementById("tasks");
+  ul.appendChild(li);
+}
